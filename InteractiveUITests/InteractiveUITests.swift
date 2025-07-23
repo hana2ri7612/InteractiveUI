341bc74c27134f6e894c8197cd8fba86e7e4e3f2
Hana Ri <hanacokeeri76@gmail.com>
2025-07-23 14:27:45 -0700

    Initial Commit

---
+++ InteractiveUITests/InteractiveUITests.swift
@@ -0,0 +1,16 @@
+//
+//  InteractiveUITests.swift
+//  InteractiveUITests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+
+struct InteractiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
