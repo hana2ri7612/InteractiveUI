44f07515d5af765525d78b61c9fa598cae85951c
Hana Ri <hanacokeeri76@gmail.com>
2025-07-23 15:06:59 -0700

    Added multiple variables such as  (name) & (textTitle). Also added action to button which utilized interpolation to change the beginning text.

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    
     var body: some View {
         VStack {
-            Text ("What is your name?")
+            Text (textTitle)
                 .font(.title)
                 .fontWeight(.semibold)
-            TextField("Type name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type name here", text: $name)
                 .multilineTextAlignment(.center)
                     .font(.title)
                     .border(Color.gray, width: 1)
             Button("Submit Name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                textTitle = "Welcome, \(name)!"
             }//closes button
-        
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.teal)
             
         }
         .padding()
